addec65fc490a015ff0a21f4028fcac2cf274172
VolodymyrHlyvka <vhlyvka@gmail.com>
2024-10-23 19:12:47 +0300

    fix service worker first load

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -7,41 +7,61 @@ const urlsToCache = [
   "/logo192.png",
   "/logo512.png",
   "/favicon.ico",
-  "/static/js/bundle.js",
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+      .catch((e) => {
+        console.error("install error", e);
+      })
   );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((cacheName) => cacheName !== CACHE_NAME)
-          .map((cacheName) => caches.delete(cacheName))
-      );
-    })
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames
+            .filter((cacheName) => cacheName !== CACHE_NAME)
+            .map((cacheName) => caches.delete(cacheName))
+        );
+      })
+      .catch((e) => {
+        console.error("activate error", e);
+      })
   );
+
+  return self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return (
-        response ||
-        fetch(event.request).then((fetchedResponse) => {
-          return caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, fetchedResponse.clone());
-            return fetchedResponse;
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request)
+        .then((networkResponse) => {
+          const responseToCache = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
           });
+
+          return networkResponse;
         })
-      );
+        .catch((error) => {
+          console.error("Fetch error:", error);
+          throw error;
+        });
     })
   );
 });
