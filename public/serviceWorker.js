88c40adaf3d97c5ba18d6b7311d3debb223b259f
VolodymyrHlyvka <148061159+VolodymyrHlyvka@users.noreply.github.com>
2024-10-23 18:23:41 +0300

    EX-4 - Sw fix (#4)

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -7,41 +7,55 @@ const urlsToCache = [
   "/logo192.png",
   "/logo512.png",
   "/favicon.ico",
-  "/static/js/bundle.js",
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+      .catch((e) => {
+        console.error("install error", e);
+      })
   );
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((cacheName) => cacheName !== CACHE_NAME)
-          .map((cacheName) => caches.delete(cacheName))
-      );
-    })
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames
+            .filter((cacheName) => cacheName !== CACHE_NAME)
+            .map((cacheName) => caches.delete(cacheName))
+        );
+      })
+      .catch((e) => {
+        console.error("activate error", e);
+      })
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return (
-        response ||
-        fetch(event.request).then((fetchedResponse) => {
-          return caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, fetchedResponse.clone());
-            return fetchedResponse;
-          });
-        })
-      );
-    })
+    caches
+      .match(event.request)
+      .then((response) => {
+        return (
+          response ||
+          fetch(event.request).then((fetchedResponse) => {
+            return caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, fetchedResponse.clone());
+              return fetchedResponse;
+            });
+          })
+        );
+      })
+      .catch((e) => {
+        console.error("fetch error", e);
+      })
   );
 });
